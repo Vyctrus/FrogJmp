592fca05d623c0f4a4e1c6d668bd13f37f3434df
Vyctrus <and.gierlak@gmail.com>
2023-04-16 15:16:02 +0200

    Thinking was done, simple,bad O(N^2) solution, and good O(N) theory added

--- src/main/java/org/example/TapeEquilibrium.java
+++ src/main/java/org/example/TapeEquilibrium.java
@@ -6,6 +6,27 @@ public class TapeEquilibrium {
         return -1;
     }
 
+    public int badSolution(int[] A){
+        //naiwny algorytm, który od razu przychodzi do głowy
+        //1)licz roznice pomiedzy poszczegolnymi elementami dla P = 1.. A.length
+        //2)wybierz minimum
+        //3)return minimum
+        //O(N^2)
+        return -1;
+    }
+
+    public int goodSolution(int[] A){
+        //przesuwaj granice tak aby byc jak najblizej polowy
+        //zsumuj wsyzsktie elmenty, podziel na 2
+        //dodawaj kolejno elementy od lewa do prawa
+        //warunek while (x< polowa)
+        //sprawdzic wartosci graniczne
+        //O(N) good
+        return -1;
+    }
+
+
+
 
 
 }
