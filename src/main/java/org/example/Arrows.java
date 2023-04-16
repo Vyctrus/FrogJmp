7959eabd616936f69630f1b88fc44f747c7a67b4
Vyctrus <and.gierlak@gmail.com>
2023-04-16 22:27:28 +0200

    Arrows seems to work, fast code,can be not efficient

--- src/main/java/org/example/Arrows.java
+++ src/main/java/org/example/Arrows.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Arrows {
     public int solution(String S){
-        return -1;
+        char[] tabChar= S.toCharArray();
+        int directions[] ={0,0,0,0};
+        for(int i=0;i<tabChar.length;i++){
+            switch (tabChar[i]){
+                case('^'):
+                    directions[0]++;
+                    break;
+                case('>'):
+                    directions[1]++;
+                    break;
+                case('v'):
+                    directions[2]++;
+                    break;
+                case('<'):
+                    directions[3]++;
+                    break;
+            }
+        }
+        int maxElements=0;
+        for(int i=0;i<directions.length;i++){
+            maxElements= (directions[i]>maxElements)? directions[i] : maxElements;
+        }
+        return tabChar.length-maxElements;
     }
 }
